e264edfe3f5ec6e9a447cb8f794673010fa68951
John <johnzhengyuliu@gmail.com>
2023-02-19 09:17:27 -0600

    switch to public devnet endpoint

--- utils/anchor-grizzly.ts
+++ utils/anchor-grizzly.ts
@@ -11,9 +11,10 @@ import { clusterApiUrl, Connection, Keypair, PublicKey } from "@solana/web3.js"
 import { findProgramAddressSync } from "@project-serum/anchor/dist/cjs/utils/pubkey"
 
 // Create a connection to the devnet cluster
-export const connection = new Connection(
-  process.env.NEXT_PUBLIC_DEVNET_ENDPOINT!
-)
+export const connection = new Connection(clusterApiUrl("devnet"))
+// export const connection = new Connection(
+//   process.env.NEXT_PUBLIC_DEVNET_ENDPOINT!
+// )
 
 // Create a placeholder wallet to set up AnchorProvider
 const wallet = new NodeWallet(Keypair.generate())
